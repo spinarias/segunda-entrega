70b9e5ce95f9236bbc01c315b8e6f5b354205b26
Manuel Spina <spina.arias@gmail.com>
2022-01-21 15:03:31 -0300

    segunda entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css';
+
+function NavBar() {
+    return (
+    <div className='NavBar-contenedor'>
+        <h1>Heladería</h1>
+        <ul>
+            <li>Gustos de helado</li>
+            <li>Chocolates</li>
+            <li>Contacto</li>
+        </ul>
+    </div>
+    );
+}
+
+export default NavBar;
